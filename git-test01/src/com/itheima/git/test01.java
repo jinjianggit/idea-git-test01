d24f02c35dfe5afcb8b12213d285bc53a1160cf8
jinjiang007 <414114807@qq.com>
2019-01-17 17:16:53 +0800

    把本地工程添加git

---
+++ git-test01/src/com/itheima/git/test01.java
@@ -0,0 +1,8 @@
+package com.itheima.git;
+
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
